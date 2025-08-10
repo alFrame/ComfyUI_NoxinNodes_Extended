a60cac90cbd49f25c80a811b26b5fbaf9ee67f9a
Alex Furer <alex@fullframestudios.ch>
2025-08-11 01:12:10 +0200

    Another round of fundamental file structuring and renaming things.

--- __init__.py
+++ __init__.py
@@ -1,29 +1,41 @@
+# Import from your actual AF modules
+from .af_load_prompt_history import *
+from .af_save_prompt_history import *
+
+# Import from existing noxin modules (if they still exist)
 from .noxin_chimenode import *
 from .noxin_scaledresolution import *
-from .noxin_promptlibrary import *
 from .noxin_saveprompt import *
 from .noxin_simplemath import *
 from .noxin_splitprompt import *
 
-
-
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
+    # AF Nodes
+    "AFPromptLoad": AFPromptLoad,
+    "AFPromptSave": AFPromptSave,
+    "AFPromptSearch": AFPromptSearch,
+    "AFPromptYAMLManager": AFPromptYAMLManager,
+    
+    # Original Noxin Nodes (if they still exist)
     "NoxinChime": NoxinChime,
     "NoxinScaledResolution": NoxinScaledResolution,
-    "NoxinPromptLoad": NoxinPromptLoad,
-    "NoxinPromptSave": NoxinPromptSave,
     "NoxinSimpleMath": NoxinSimpleMath,
     "NoxinSplitPrompt": NoxinSplitPrompt
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
+    # AF Nodes
+    "AFPromptLoad": "AF Load Prompt History",
+    "AFPromptSave": "AF Save Prompt History", 
+    "AFPromptSearch": "AF Prompt Search",
+    "AFPromptYAMLManager": "AF Prompt YAML Manager",
+    
+    # Original Noxin Nodes (if they still exist)
     "NoxinChime": "Noxin Complete Chime",
     "NoxinScaledResolution": "Noxin Scaled Resolutions",
-    "NoxinPromptLoad": "Load from Noxin Prompt Library",    
-    "NoxinPromptSave": "Save to Noxin Prompt Library",
     "NoxinSimpleMath": "Simple Math Operations",
     "NoxinSplitPrompt": "Split Prompt Organiser"
 }
